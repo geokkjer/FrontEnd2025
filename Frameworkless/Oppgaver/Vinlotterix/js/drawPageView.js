1f2fa3ace1ea32460294be7ef13884dadf86cd5c
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-02-25 09:22:59 +0100

    dag 2

--- Frameworkless/Oppgaver/Vinlotterix/js/drawPageView.js
+++ Frameworkless/Oppgaver/Vinlotterix/js/drawPageView.js
@@ -1,12 +1,25 @@
-import { getDateStringForDisplay, getDateStringForStorage, getNowForStorage } from "./common.js";
-import { selectAllOrNone, addPerson, togglePersonSelected, deletePerson, draw, changeDrawCount } from "./drawPageController.js";
+import {
+  getDateStringForDisplay,
+  getDateStringForStorage,
+  getNowForStorage,
+} from "./common.js";
+import {
+  selectAllOrNone,
+  addPerson,
+  togglePersonSelected,
+  deletePerson,
+  draw,
+  changeDrawCount,
+} from "./drawPageController.js";
 import model from "./model.js";
 
 function updateViewDrawPage() {
   let html = `<table>
                   <tr>
                     <td><input type="checkbox"
-                               ${getChecked(model.inputs.drawPage.selectAll)}/></td>
+                               ${getChecked(
+                                 model.inputs.drawPage.selectAll
+                               )}/></td>
                     <td><b>Personer</b></td>
                     <td>
                     </td>
@@ -16,7 +29,9 @@ function updateViewDrawPage() {
                   <td><input type="checkbox"                             
                              ${getChecked(person.isSelected)}"/></td>
                   <td>${person.name}</td>
-                  <td><button class="litenKnapp"  onclick="deletePerson(${person.id})">x</button></td>
+                  <td><button class="litenKnapp"  onclick="deletePerson(${
+                    person.id
+                  })">x</button></td>
                 </tr>`;
   }
   html += `<tr>
@@ -45,50 +60,57 @@ function updateViewDrawPage() {
                 </td>
               </tr>
             </table>`;
-  document.getElementById('app').innerHTML = `
+  document.getElementById("app").innerHTML = `
         <div class="page">
             <div class="header">
                 Vinlotterix 🍷
             </div>
             <div class="innhold">${html}</div>
             <div class="meny">
-                <button class="knapp fixed" onclick="model.app.currentPage='draw'; updateView()">Personer</button><br />
-                <button class="knapp fixed" onclick="model.app.currentPage='winners'; updateView()">Vinnere</button><br />
+                <button id="draw" class="knapp fixed">Personer</button><br />
+                <button id="winners" class="knapp fixed">Vinnere</button><br />
             </div>
         </div>  
     `;
-  let checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+  let checkboxes = Array.from(
+    document.querySelectorAll('input[type="checkbox"]')
+  );
   const firstCheckbox = checkboxes.shift();
   // console.log(checkboxes, firstCheckbox);
   // onclick="selectAllOrNone(this.checked)"
-  firstCheckbox.addEventListener('click', () => selectAllOrNone(firstCheckbox.checked));
+  firstCheckbox.addEventListener("click", () =>
+    selectAllOrNone(firstCheckbox.checked)
+  );
   for (let i = 0; i < checkboxes.length; i++) {
     let checkbox = checkboxes[i];
-    // onclick="togglePersonSelected(${person.id})" 
+    // onclick="togglePersonSelected(${person.id})"
     const person = model.inputs.drawPage.list[i];
-    checkbox.addEventListener('click', () => togglePersonSelected(person.id));
+    checkbox.addEventListener("click", () => togglePersonSelected(person.id));
   }
   let textbox = document.querySelector('input[type="text"]');
-  // oninput="model.inputs.drawPage.newPersonName=this.value" 
-  textbox.addEventListener('input', () => model.inputs.drawPage.newPersonName = textbox.value)
+  // oninput="model.inputs.drawPage.newPersonName=this.value"
+  textbox.addEventListener(
+    "input",
+    () => (model.inputs.drawPage.newPersonName = textbox.value)
+  );
 
   // <button id="addBtn" class="litenKnapp" onclick="addPerson()">legg til person</button>
-  const addBtn = document.getElementById('addBtn');
-  addBtn.addEventListener('click', addPerson);
+  const addBtn = document.getElementById("addBtn");
+  addBtn.addEventListener("click", addPerson);
   // let numberInput = document.querySelectorAll('input[type="number"]');
+
+  let drawButton = document.querySelector('button[id="draw"]');
+  let winnerButton = document.querySelector('button[id="winners"]');
+  drawButton.addEventListener("click", () => setPageview("draw"));
+  winnerButton.addEventListener("click", () => setPageview('winners'));
 }
 
 function getChecked(isSelected) {
-  return isSelected ? 'checked="checked"' : '';
+  return isSelected ? 'checked="checked"' : "";
 }
-let buttons = Array.from(document.querySelectorAll('button[class="knapp fixed"]'))
-console.log(buttons)
-function setCurrentPage() {
-  
-  for (let i = 0; i < buttons.length; i++){
-    let button = buttons[i]
-    let currentPage = model.app.currentPage
-    button.addEventListener('click')
-  }
-} 
+function setPageview(page) {
+  console.log(page)
+  model.app.currentPage = page;
+  window.updateView();
+}
 export default updateViewDrawPage;
