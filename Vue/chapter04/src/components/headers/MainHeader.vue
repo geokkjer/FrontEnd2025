faa4cc7d68d13db505191c89cf602107ba071f50
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-04-30 09:01:31 +0200

    todo app

---
+++ Vue/chapter04/src/components/headers/MainHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header class="w3-blue w3-padding">
+        <i class="fa-solid fa-fw fa-list"></i>
+        To-Do List
+    </header>
+</template>
+
+<style scoped></style>
--- Vue/chapter04/src/main.ts
+++ Vue/chapter04/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import Modals from "./plugins/modals"
+// import styles from './assets/styles.css'
+
 
 createApp(App)
     .use(Modals)
