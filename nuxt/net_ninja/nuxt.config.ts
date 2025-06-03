292d525979860f0a065c48ea7ba5e72e455afa86
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-06-03 10:49:48 +0200

    learning nuxt, and tailwind v4

--- nuxt/net_ninja/nuxt.config.ts
+++ nuxt/net_ninja/nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', 'nuxt-mcp', '@nuxtjs/tailwindcss']
+  modules: ['@nuxt/fonts', '@nuxt/icon', 'nuxt-mcp', '@nuxtjs/tailwindcss'],
 })
